ac68e1b0993d50b665cde0a153a5b354842d9d89
NYO <126266679+rosemaryuser@users.noreply.github.com>
2024-05-28 17:01:28 +0800

    bootctrl: Update to version 1.2
    * Implement getActiveBootSlot

--- device.mk
+++ device.mk
@@ -61,7 +61,7 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-mtkimpl.recovery
+    android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
