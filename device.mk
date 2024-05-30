bb8fb567fc5cc29c62a73144e59a5e243d0d2693
NYO <126266679+rosemaryuser@users.noreply.github.com>
2024-05-30 21:14:47 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -62,6 +62,7 @@ PRODUCT_PACKAGES += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.2-mtkimpl
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
